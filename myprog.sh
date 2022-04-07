b8f674502490b07584084a6589b1068dd26ac6e1
ImChaeha <87749460+ImChaeha@users.noreply.github.com>
2022-04-07 22:54:23 +0900

    Update myprog.sh

--- myprog.sh
+++ myprog.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 PS3="select menu: "
-if [ ! -d "temp "]
+if [ ! -d "temp " ]
 then
 	mkdir temp
 	echo "...create temp directory..."
 fi
+chmod 777 mycal.sh
 mv num1.txt num2.txt mycal.sh temp
 echo "...copy files to temp directory..."
 cd temp
@@ -30,4 +31,3 @@ do
 done
 ./mycal.sh $var
 exit 0
-
